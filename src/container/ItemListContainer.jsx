2a4a3e91b23c1cd923985f8604bf78455e547a04
Gabriel Vasilev <vasilevgabriel@gmail.com>
2021-11-17 00:06:49 -0300

    Listar productos

--- src/container/ItemListContainer.jsx
+++ src/container/ItemListContainer.jsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react"
 import { ItemCount } from "../components/ItemCount"
+import { ItemList } from "../components/ItemList"
+import { pedirDatos } from "../helpers/pedirDatos"
 import { ProductCard } from "../components/ProductCard"
 
-export const ItemListContainer = (mensaje) =>{
+export const ItemListContainer = () =>{
+
+    const [loading, setLoading] = useState(false)
+    const [productos, setProductos] = useState([])
+
+    useEffect(() =>{
+
+        setLoading(true)
+        pedirDatos()
+            .then((resp) =>{
+                setProductos(resp)
+            })
+            .catch((error) =>{
+                console.log(error)
+            })
+            .finally(()=>{
+                setLoading(false)
+            })
+
+    }, [])
 
     return(
         <div>
@@ -9,6 +31,12 @@ export const ItemListContainer = (mensaje) =>{
             <hr />
             <ProductCard name="Producto" img={productoFoto} stock="10"/>
             <ItemCount/>
+
+            {
+                loading
+                ? <h2>Cargando..</h2>
+                : <ItemList productos={productos}/>
+            }
         </div>
     )
 
---
+++ src/data/stock.js
@@ -0,0 +1,38 @@
+export const stock =[
+
+{
+    id: 1,
+    price: 1500,
+    name: "Producto Uno",
+    desc: "Descripcion producto",
+    img: "http://www.google.com/img.jpg"
+},
+{
+    id: 2,
+    price: 1500,
+    name: "Producto Uno",
+    desc: "Descripcion producto",
+    img: "http://www.google.com/img.jpg"
+},
+{
+    id: 3,
+    price: 1500,
+    name: "Producto Uno",
+    desc: "Descripcion producto",
+    img: "http://www.google.com/img.jpg"
+},
+{
+    id: 4,
+    price: 1500,
+    name: "Producto Uno",
+    desc: "Descripcion producto",
+    img: "http://www.google.com/img.jpg"
+},
+{
+    id: 5,
+    price: 1500,
+    name: "Producto Uno",
+    desc: "Descripcion producto",
+    img: "http://www.google.com/img.jpg"
+}
+]
